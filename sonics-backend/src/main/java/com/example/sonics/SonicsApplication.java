81487f22c418cd80380f9aa84a62e0cb3729061a
Knightwing1941 <pavik14@live.com>
2026-04-27 13:57:42 -0500

    initial commit

---
+++ sonics-backend/src/main/java/com/example/sonics/SonicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonicsApplication.class, args);
+	}
+
+}
---
+++ sonics-backend/src/test/java/com/example/sonics/SonicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
