f23ab30218b3fc20768806215a2d7e936ff615fa
Knightwing1941 <pavik14@live.com>
2026-05-07 15:39:30 -0500

    Remove unused method from repository file

--- sonics-backend/src/main/java/com/example/sonics/controller/Team_Store_ProductController.java
+++ sonics-backend/src/main/java/com/example/sonics/controller/Team_Store_ProductController.java
@@ -1,14 +1,11 @@
 package com.example.sonics.controller;
 
-import com.example.sonics.model.Staff;
 import com.example.sonics.model.Team_Store_Product;
-import com.example.sonics.service.StaffService;
 import com.example.sonics.service.Team_Store_ProductService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
